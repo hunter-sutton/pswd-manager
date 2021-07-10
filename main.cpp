56c343e1e32fbbc7957f6889f5005dea4317c618
Hunter Sutton <suttonhu@oregonstate.edu>
2021-07-09 19:50:15 -0700

    main now uses new functions added to file_interact and userinterface

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "userInterface.h"
+#include "file_interact.h"
 
 using namespace std;
 
@@ -9,6 +10,12 @@ int main() {
   while (userInput != 3) {
     printOptions();
     cin >> userInput;
+    if (userInput == 1) {
+      writeNewLoginInfo(getNewLoginPass(), getNewLoginUsername(), getNewLoginName());
+    }
+    else if (userInput == 2) {
+      //let user edit some desired login info
+    }
   }
 
   return 0;
